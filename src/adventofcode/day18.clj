6ec5b1d58970525fbb188dbe3fd7971bb162a71d
Tom Cooper <tcooper@frontlineed.com>
2018-02-25 18:51:22 -0500

    part 2

--- src/adventofcode/day18.clj
+++ src/adventofcode/day18.clj
@@ -1,4 +1,5 @@
-(ns adventofcode.day18)
+(ns adventofcode.day18
+  (:require [clojure.core.async :refer [>! >!! <! <!! go chan buffer close! thread alts! alts!! timeout]]))
 
 ;; Advent of Code challenges
 ;; http://adventofcode.com/2017/day/18
@@ -9,7 +10,7 @@
 
 (def sample "set a 1\nadd a 2\nmul a a\nmod a 5\nsnd a\nset a 0\nrcv a\njgz a -1\nset a 1\njgz a -2")
 
-;; Value references
+;; Values based on the reference type
 (defmulti value-of (fn [_ x] (number? x)))
 
 (defmethod value-of true [computer x] x)
@@ -46,16 +47,18 @@
       (assoc computer :halted true))
     computer))
 
-(defn jgz-fn [computer arg1 arg2]
-  (if-not (zero? (value-of computer arg1))
-    (assoc computer :pc (+ (value-of computer arg2) (dec (:pc computer))))
+(defn jgz-fn [computer x y]
+  (if (> (value-of computer x) 0)
+    (assoc computer :pc (+ (value-of computer y) (dec (:pc computer))))
     computer))
 
-(def part1-computer
-  {:played nil
-   :registers {}
-   :pc 0
-   :instruction-set {"snd" snd-fn, "set" set-fn, "add" add-fn, "mul" mul-fn, "mod" mod-fn, "rcv" rcv-fn, "jgz" jgz-fn}})
+; Create a computer prototype
+(def computer-prototype
+  {:played          nil
+   :registers       {}
+   :pc              0
+   :instruction-set {"snd" snd-fn, "set" set-fn, "add" add-fn, "mul" mul-fn,
+                     "mod" mod-fn, "rcv" rcv-fn, "jgz" jgz-fn}})
 
 ; Program
 (defn compile-instruction [computer s]
@@ -69,34 +72,52 @@
   (zipmap (range) (map (partial compile-instruction computer) (clojure.string/split-lines code))))
 
 (defn run-code [computer program]
-  (let [{:keys [inst-fn arg1 arg2]} (get program (:pc computer))]
-    (if-not (or (nil? inst-fn) (:halted computer))
-      (recur (inst-fn (assoc computer :pc (inc (:pc computer))) arg1 arg2)))))
+  (loop [next-state (assoc computer :executed 0)]
+    (let [{:keys [inst-fn arg1 arg2]} (get program (:pc next-state))]
+      (if-not (or (nil? inst-fn) (:halted next-state))
+        (recur (assoc (inst-fn (assoc next-state :pc (inc (:pc next-state))) arg1 arg2)
+                 :executed (inc (:executed next-state))))
+        next-state))))
 
 (defn part1
-  ([infile] (part1 part1-computer infile))
-  ([computer infile]
-   (->> infile slurp (compile-program computer) (run-code computer))))
+  ([infile] (part1 computer-prototype (slurp infile)))
+  ([computer program]
+   (->> program (compile-program computer) (run-code computer))))
 
 ; (part1 input-file))
 ; Recovered sound: 8600
 
-(defn send-fn [computer arg _]
-  ; See concurrency book for best hand-off mechanism. Probably core.async channel.
-  computer)
+;; Part 2
+;; snd rcv is async send/receive
 
-(defn receive-fn [computer arg _]
-  ; See concurrency book for best hand-off mechanism. Probably core.async channel.
-  ; TODO How do you detect deadlock here?
-  computer)
+; Two new instruction implementations
+(defn send-fn [computer arg _]
+  (>!! (:write-chan computer) (value-of computer arg))
+  (assoc computer :sent-count (inc (get computer :sent-count 0))))
+
+(defn receive-fn [computer register _]
+  (if-let [x (first (alts!! [(:read-chan computer) (timeout 2000)]))]
+    (assoc-in computer [:registers register] x)
+    (assoc computer :halted true)))
+
+; Construct a part2 computer to override those instructions and provide read and write channels
+(defn part2-computer [id read-chan write-chan]
+  (assoc computer-prototype
+    :instruction-set (merge (:instruction-set computer-prototype) {"snd" send-fn, "rcv" receive-fn})
+    :registers (merge (:registers computer-prototype) {'p id})
+    :read-chan read-chan
+    :write-chan write-chan
+    :id id))
 
 (defn part2 [infile]
-  (let [computer2 (assoc part1-computer
-                    :instruction-set (merge (:instruction-set part1-computer) {"snd" send-fn, "rcv" receive-fn}))]
-    (part1 computer2 infile)))
+  (let [chan0 (chan 100) chan1 (chan 100)
+        program (slurp infile)
+        computer0 (part2-computer 0 chan0 chan1)
+        computer1 (part2-computer 1 chan1 chan0)]
+    (thread (part1 computer0 program))
+    (println "Computer 1 ran"
+             (:sent-count (part1 computer1 program))
+             "instructions")))
 
-
-;; Part 2
-;; Position 2 changes every time the spin wraps around, when the mod = 0
-
-
+; (part2 (slurp input-file))
+; Computer 1 ran 7239 instructions
