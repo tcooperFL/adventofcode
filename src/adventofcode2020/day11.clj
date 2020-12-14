603d2955c0838ad89a2471cd1177e9a4ea583260
Tom Cooper <tcooper@frontlineed.com>
2020-12-14 00:04:23 -0500

    day 11 part 2

--- src/adventofcode2020/day11.clj
+++ src/adventofcode2020/day11.clj
@@ -8,45 +8,88 @@
   (with-open [rdr (clojure.java.io/reader file)]
     (into [] (map vec) (line-seq rdr))))
 
-(defn inspect [[m r c]] (get-in m [r c]))
+(defn location [room r c] [room r c])
 
-(defn occupied? [p] (= (inspect p) \#))
+(defn inspect [[room r c]] (get-in (:positions room) [r c]))
 
-(defn surrounding [[m r c]]
-  (map (fn [[dx dy]] [m (+ r dx) (+ c dy)])
-       (for [dx [-1 0 1] dy [-1 0 1] :when (not= [0 0] [dx dy])] [dx dy])))
+(defn occupied? [loc] (= (inspect loc) \#))
 
-(defn occupied-adjacent [p]
-  (count (filter occupied? (surrounding p))))
+(defn move [[room r c] fx fy] [room (fx r) (fy c)])
+
+(def directions
+  (for [dx [dec identity inc] dy [dec identity inc]
+        :when (not= [identity identity] [dx dy])] [dx dy]))
+
+(defn surrounding [[room r c]]
+  (map (fn [[fx fy]] (move [room r c] fx fy)) directions))
+
+(defn occupied-adjacent [loc]
+  (count (filter occupied? (surrounding loc))))
 
 (defn tick
   "Create the next state for this position"
-  [p]
-  (condp = (inspect p)
-    \L (if (zero? (occupied-adjacent p)) \# \L)
-    \# (if (>= (occupied-adjacent p) 4) \L \#)
-    (inspect p)))
+  [[room :as loc]]
+  (condp = (inspect loc)
+    \L (if ((:roomy? room) loc) \# \L)
+    \# (if ((:crowded? room) loc) \L \#)
+    (inspect loc)))
 
 (defn round
   "Create the next round of the seating state"
-  [m]
-  (mapv (fn [r]
-         (mapv (fn [c] (tick [m r c]))
-              (range (count (first m)))))
-       (range (count m))))
+  [{:keys [rows columns generation] :as room}]
+  (assoc room
+    :generation (inc generation)
+    :positions
+    (mapv (fn [r]
+            (mapv (fn [c] (tick (location room r c)))
+                  (range columns)))
+          (range rows))))
 
-(defn total-occupied [m]
-  (count (filter #{\#} (flatten m))))
+(defn total-occupied
+  "Count the number of occupied seats in the room"
+  [room]
+  (count (filter #{\#} (flatten (:positions room)))))
 
-(defn solve1 [m]
-  (total-occupied (ffirst (drop-while (fn [[m1 m2]] (not= m1 m2))
-                                      (partition 2 1 (iterate round m))))))
+(defn create-room [positions]
+  {:positions  positions
+   :rows       (count positions)
+   :columns    (count (first positions))
+   :roomy?     #(zero? (occupied-adjacent %))
+   :crowded?   #(>= (occupied-adjacent %) 4)
+   :generation 0})
+
+(defn solve1
+  "Keep iterating on rounds until positions don't change."
+  [room]
+  (total-occupied
+    (ffirst (drop-while (fn [[r1 r2]] (not= (:positions r1) (:positions r2)))
+                        (partition 2 1 (iterate round room))))))
 
 (defn part1 [file]
-  (solve1 (get-data file)))
+  (solve1 (create-room (get-data file))))
 
 ; (time (part1 input-data))
 ; "Elapsed time: 3832.046739 msecs"
 ; => 2344
 
+(defn in-sight
+  "What is the first non-empty item in each direction?"
+  [location]
+  (map (fn [[fx fy]]
+         (first
+           (drop-while #(= (inspect %) \.) (rest (iterate #(move % fx fy) location)))))
+       directions))
 
+(defn occupied-in-sight
+  "How many occupied seats can I see in all directions at any distance?"
+  [loc]
+  (count (filter occupied? (in-sight loc))))
+
+(defn part2 [file]
+  (solve1 (merge (create-room (get-data file))
+                 {:roomy?   #(zero? (occupied-in-sight %))
+                  :crowded? #(>= (occupied-in-sight %) 5)})))
+
+; (time (part2 input-data))
+; "Elapsed time: 5456.393414 msecs"
+; => 2076
