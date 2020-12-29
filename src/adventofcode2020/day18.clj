31621dc5148bfb90a40ebc1d8b1f29cd1e0b226a
Tom Cooper <tcooper@frontlineed.com>
2020-12-28 21:44:24 -0500

    day 18 part 2

--- src/adventofcode2020/day18.clj
+++ src/adventofcode2020/day18.clj
@@ -1,5 +1,6 @@
 (ns adventofcode2020.day18
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.walk :refer :all]))
 
 (def input-data "resources/2020/day18-input.txt")
 
@@ -7,33 +8,64 @@
   (with-open [rdr (clojure.java.io/reader file)]
     (f (line-seq rdr))))
 
-(def operation {"+" + "*" *})
+(def ^:dynamic prec<
+  "Default precedence is left associativity"
+  (fn [_ _] false))
 
 (defn tokenize [s]
   (map #(or (get {"+" "+" "*" "*" "(" :begin ")" :end} %)
             (Integer/parseInt %))
        (s/split (s/replace s #"\s+" "") #"(?=\d+|\s+|[+\\*\\(\\)])")))
 
-(defn push-value [v [top & tail] new-top]
-  (if (string? (first top))
-    (let [result ((operation (first top)) (second top) v)]
-      (cons (cons result new-top) tail))
-    (cons (cons v top) tail)))
+(defn push-value [v [top & tail]]
+  (if (< (count top) 3)
+    (cons (conj top v) tail)
+    (let [[op arg1 arg2] top]
+      (cons [op arg1 (first (push-value v (list arg2)))] tail))))
 
-(defn evaluate [s]
-  (ffirst (reduce (fn [[top & tail :as stack] term]
-                    (cond (= :begin term) (cons nil stack)
-                          (= :end term) (push-value (first top) tail top)
-                          (string? term) (cons (cons term top) tail)
-                          (number? term) (push-value term stack (drop 2 top))))
-                  '(())
-                  (tokenize s))))
+(defn push-function [term [top & tail]]
+  (if (number? top)
+    (cons [term top] tail)
+    (let [[op arg1 arg2] top]
+      (cond (number? op) (cons [term op] tail)
+            (prec< op term) (cons [op arg1 (first (push-function term [arg2]))] tail)
+            :else (cons [term top] tail)))))
+
+(defn parse
+  "Parse the string into an AST"
+  [s]
+  (first (reduce (fn [[cur & pushed :as stack] term]
+                   (cond (= :begin term) (cons nil stack)
+                         (= :end term) (push-value (list cur) pushed)
+                         (string? term) (push-function term stack)
+                         (number? term) (push-value term stack)))
+                 '(nil)
+                 (tokenize s))))
+
+(defn evaluate
+  "Walk the AST to evaluate it"
+  [ast]
+  (let [operation {"+" + "*" *}]
+    (postwalk #(cond (not (coll? %)) %
+                    (number? (first %)) (first %)
+                    :else (apply (operation (first %)) (rest %)))
+              ast)))
 
 (defn solve [lines]
-  (reduce + (map evaluate lines)))
+  (->> lines (map parse) (map evaluate) (reduce +)))
 
 (defn part1 [file]
   (process-file solve file))
 
 ; (part1 input-data)
 ; => 45840336521334
+
+(defn part2
+  "Change operator precedence and run part1 again."
+  [file]
+  (binding [prec< (fn [f1 f2] (and (= f1 "*") (= f2 "+")))]
+    (part1 file)))
+
+; (time (part2 input-data))
+; "Elapsed time: 25.850787 msecs"
+; => 328920644404583
