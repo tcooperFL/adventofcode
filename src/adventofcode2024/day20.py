89f9a430e403ed02a89092bbacff9b05eee80dea
Tom Cooper <tcooper@frontlineed.com>
2024-12-30 17:29:41 -0500

    Day 20

--- src/adventofcode2024/day20.py
+++ src/adventofcode2024/day20.py
@@ -1,7 +1,7 @@
 # Link: https://adventofcode.com/2024/day/20
 
-from pprint import pprint
 from utils import timed
+from itertools import product
 
 INPUT_FILE = "resources/2024/day20-input.txt"
 
@@ -46,40 +46,38 @@ def find_path(track):
     return []
 
 
-def calculate_cheats(track, path):
+def calculate_cheats(track, path, distance=20):
     timings = {pos: i for i, pos in enumerate(path)}
     cheats = dict()
     for (py, px), moment in timings.items():
-        for dy, dx in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            if (py + dy, px + dx) in track["blocked"]:
-                landing = (py + 2 * dy, px + 2 * dx)
+        for dy, dx in product(range(-distance, distance + 1), repeat=2):
+            steps = abs(dy) + abs(dx)
+            if 0 < steps <= distance:
+                landing = (py + dy, px + dx)
                 new_moment = timings.get(landing, -1e9)
-                if new_moment > moment:
-                    savings = (new_moment - moment) - 2  # 2 is the cost of the jump
+                savings = (new_moment - moment) - steps
+                if savings >= 100:
                     cheats[savings] = cheats.get(savings, 0) + 1
 
-    return cheats
+    return sum(cheats[savings] for savings in cheats)
 
 
 @timed
 def part1():
     track = get_track()
     path = find_path(track)
-    cheats = calculate_cheats(track, path)
-    return sum(cheats[savings] for savings in cheats if savings >= 100)
-
-
-# Part 2
+    cheats = calculate_cheats(track, path, distance=2)
+    return cheats
 
 
 @timed
 def part2():
-    # TODO: Inside calculate_cheats we look each direction where there is a wall
-    # and then we check do another search up to 20 steps and collect all cheat
-    # savings.
-    return "tbd"
+    track = get_track()
+    path = find_path(track)
+    cheats = calculate_cheats(track, path, distance=20)
+    return cheats
 
 
 if __name__ == "__main__":
-    pprint(part1())
-    # print(part2())
+    print(part1())
+    print(part2())
